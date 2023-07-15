a2f052973b575f01b09e85e597b1797e28f0a4f8
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-15 15:32:10 +0900

    Revert "一旦削除"
    This reverts commit 6a1604cbb9c679f0ed5d48eba45a62282a008170.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_scope :user do
-    get 'users/edit/name', to: 'devise/registrations#edit_name', as: 'edit_name'
-    put 'users/update/name', to: 'devise/registrations#update_name', as: 'update_name'
-    get 'users/edit/address', to: 'devise/registrations#edit_address', as: 'edit_address'
-    put 'users/update/address', to: 'devise/registrations#update_address', as: 'update_address'
-    get 'users/edit/password', to: 'devise/registrations#edit_password', as: 'edit_password'
-    put 'users/update/password', to: 'devise/registrations#update_password', as: 'update_password'
-  end
 
   root to: "cares#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2023_07_14_121056) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "current_password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
