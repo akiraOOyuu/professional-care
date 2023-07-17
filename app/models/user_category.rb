9db658afdaff3e66c114114006943a00065267c9
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-17 22:45:06 +0900

    user_cutegoryバリデーション設定

--- app/models/user_category.rb
+++ app/models/user_category.rb
@@ -1,2 +1,13 @@
 class UserCategory < ApplicationRecord
+
+
+  with_options presence: true do
+    validates :employment
+    validates :affiliation,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  end
+
+  validates :rank_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :complete_id, numericality: { other_than: 1 , message: "can't be blank"}
+  
+  belongs_to :user
 end
