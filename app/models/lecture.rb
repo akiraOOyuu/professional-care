4512c52dbcdc0445c7ca9b63a16bdad79209d33d
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-24 08:35:39 +0900

    バリデーションエラー編集

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,14 +2,16 @@ class Lecture < ApplicationRecord
 
   belongs_to :user
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
-    validates :lecture_name
-    validates :instructor_name
+  with_options presence: true,  do
+   
+    
   end
 
   with_options presence: true do
     validates :lecture_day
     validates :lecture_time, format: { with: /\A[0-9]+\z/}
+    validates :lecture_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+    validates :instructor_name, format: { with: /\A[0-9０-９ぁ-んァ-ヶ一-龥・ 　ー()（）\s]+\z/ }
   end
 
   validates :field_id, numericality: { other_than: 1, message: "can't be blank" }
