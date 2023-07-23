f3e9814ba6c0c5462c29d7dc86196de95359c7ae
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-23 12:20:19 +0900

    Revert "削除"
    This reverts commit 4ba810ba616fbbaf48e574e667555f881ef75a62.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def update_resource(resource, user_params)
-    resource.update_without_password(user_params.except(:password, :password_confirmation))
-  end
-
-  def after_update_path_for(resource)
-    user_path(resource)
-  end
-end
 
   private
 
@@ -33,22 +25,6 @@ end
                                           :city, 
                                           :block, 
                                           :building])
-                                          # パスワードとパスワード確認の項目を許可リストから削除する
-      #  devise_parameter_sanitizer.permit(:account_update,
-      #                    :email, 
-      #                    :worker_prefecture_id, 
-      #                    :childcare_worker_number, 
-      #                    :childcare_worker_id, 
-      #                    :first_name, 
-      #                    :last_name, 
-      #                    :first_name_reading, 
-      #                    :last_name_reading, 
-      #                    :birth_date, 
-      #                    :postcode, 
-      #                    :prefecture_id, 
-      #                    :city, 
-      #                    :block, 
-      #                    :building,
-      #                     )
+    end
   end
-  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,31 +10,4 @@ class UsersController < ApplicationController
       
       redirect_to root_path unless @user == current_user
    end
-   def update
-      @user = User.find(params[:id])
-      if @user.valid?
-        @user.update(update_params)
-        redirect_to user_path(@user.id)
-      else
-        render :edit
-      end
-    end
-    
-    private
-    def update_params
-      params.require(:user).permit(:email, 
-        :worker_prefecture_id, 
-        :childcare_worker_number, 
-        :childcare_worker_id, 
-        :first_name, 
-        :last_name, 
-        :first_name_reading, 
-        :last_name_reading, 
-        :birth_date, 
-        :postcode, 
-        :prefecture_id, 
-        :city, 
-        :block, 
-        :building,)
-    end
 end
