24dbfb370a7fc9b4b3a67ab29d2ee483d651f669
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-25 21:33:41 +0900

    ヴァリデーション修正

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -5,8 +5,8 @@ class Lecture < ApplicationRecord
   with_options presence: true do
     validates :lecture_day
     validates :lecture_time, format: { with: /\A[0-9]+\z/}
-    validates :lecture_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
-    validates :instructor_name, format: { with: /\A[0-9０-９ぁ-んァ-ヶ一-龥・ 　ー()（）\s]+\z/ }
+    validates :lecture_name, format: { with: /\A[0-9０-９ぁ-んァ-ヶ一-龥・ 　ー()（）\s]+\z/ }
+    validates :instructor_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
   end
 
   validates :field_id, numericality: { other_than: 1, message: "can't be blank" }
