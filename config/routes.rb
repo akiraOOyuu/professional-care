9903e8ca1cda351490659a9f4a3b9a7897cade31
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-27 07:26:31 +0900

    Revert "削除"
    This reverts commit b0d7bae5409222f41d05116edcd4fc73a5068791.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :users, controllers: {
     # パス編集
     registrations: 'users/registrations'
@@ -7,7 +6,7 @@ Rails.application.routes.draw do
 
   root to: "cares#index"
 
-    # 講義内容登録・編集
+      # 講義内容登録・編集
   resources :cares, only: [:index, :new, :create,:edit, :update ] do
     collection do
       get :lecture_new
@@ -19,8 +18,8 @@ Rails.application.routes.draw do
     delete :lecture_delete
     end
   end
+  #  パス以外のユーザー情報編集
   resources :users, only: [:show] do
-    #  パス以外のユーザー情報編集
     member do
       get :edit_other_info
       patch :update_other_info
