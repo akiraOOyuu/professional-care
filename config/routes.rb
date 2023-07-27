e5735f7aeed9e37194c8ff25628bc91f2d49d76b
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-27 19:47:12 +0900

    管理者ユーザーコントローラー作成
    ルート
    コントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   devise_for :users, controllers: {
-    # パス編集
+    # 意外のuser情報編集
     registrations: 'users/registrations'
   }
 
