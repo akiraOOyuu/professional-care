45aef74eb9f047b1a94dc6bbb0439d01b8c8f57b
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-07-28 22:04:04 +0900

    admin　ログイン　サインアップ　ビュー作成
    admin　view
    CSS

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,7 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    has_many :users
+
+    validates :facility, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
 end
