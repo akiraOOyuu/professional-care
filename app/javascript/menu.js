aa40e5ce4f6e0389d268b25e2e62cc1d3aca2f97
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-08-23 13:38:21 +0900

    ハンバーガー修正

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -1,16 +1,18 @@
-function pullDown() {
-  const pullDownButton = document.getElementById("hamburger-btn");
-  const pullDownParents = document.getElementById("pull-down");
+document.addEventListener('DOMContentLoaded', function() {
+  const hamburgerButton = document.getElementById('hamburger-btn');
+  const closeButton = document.getElementById('hamburger-close-btn');
+  const pullDownMenu = document.getElementById('pull-down');
   
-
-  pullDownButton.addEventListener('click', function() {
+  hamburgerButton.addEventListener('click', function() {
     event.preventDefault(); // デフォルトの挙動をキャンセル
-    if (pullDownParents.getAttribute("style") == "display:block;") {
-      pullDownParents.removeAttribute("style");
-    } else {
-      pullDownParents.setAttribute("style", "display:block;");
-    }
+    pullDownMenu.style.display = 'block';
+    hamburgerButton.style.display = 'none';
+    closeButton.style.display = 'block';
   });
-}
 
-window.addEventListener('load', pullDown);
+  closeButton.addEventListener('click', function() {
+    pullDownMenu.style.display = 'none';
+    closeButton.style.display = 'none';
+    hamburgerButton.style.display = 'block';
+  });
+});
