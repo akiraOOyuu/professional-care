b6fdbaebd7d2c7b6e77a093315194b271995854d
akiraOOyuu <yuu.u3u.milk@gmail.com>
2023-08-27 07:57:21 +0900

    Revert "修正"
    This reverts commit 24cbf9b7b8cde8ba66acbca148d561f3b549323f.

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -27,12 +27,4 @@ WickedPdf.config = {
   # 'xvfb-run' command, in order to simulate an X server.
   #
   # use_xvfb: true,
-  :exe_path => Gem.bin_path('wkhtmltopdf-binary', 'wkhtmltopdf'),
-  :wkhtmltopdf => '/usr/local/bin/wkhtmltopdf', # この部分は wkhtmltopdf のパスを指定
-  :encoding => "utf-8", # エンコーディングを指定
-  :use_xvfb => true, # 仮想 X サーバーを使用する場合
-  :layout => "pdf_styles.html", # PDF用のレイアウトファイルを指定
-  :margin => { :top => 20, :bottom => 20, :left => 20, :right => 20 }, # ページ余白を指定
-  :footer => { :center => "Center Text", :font_size => 10 }, # フッターの設定
-  :extra => "--no-sandbox --font-family 'IPAゴシック'" # IPAゴシックのフォントファミリーを指定
 }
